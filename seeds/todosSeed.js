d7588714aef868a3a64dd92be1e0e4e2e49f03a7
Amanda Baker <mandykatebaker@Amandas-MacBook-Pro.local>
2016-08-02 20:42:56 +1200

    updated id

--- seeds/todosSeed.js
+++ seeds/todosSeed.js
@@ -5,9 +5,9 @@ exports.seed = function(knex, Promise) {
     .then(function () {
       return Promise.all([
         // Inserts seed entries
-        knex('todos').insert({done: 'true', description: 'Tidy Room', user_id: 16}),
-        knex('todos').insert({done: 'false', description: 'Get Sleep', user_id: 16}),
-        knex('todos').insert({done: 'true', description: 'Buy Groceries', user_id: 16})
+        knex('todos').insert({done: 'true', description: 'Tidy Room', user_id: 2}),
+        knex('todos').insert({done: 'false', description: 'Get Sleep', user_id: 3}),
+        knex('todos').insert({done: 'true', description: 'Buy Groceries', user_id: 4})
       ]);
     });
 };
--- seeds/usersSeed.js
+++ seeds/usersSeed.js
@@ -4,10 +4,10 @@ exports.seed = function(knex, Promise) {
     .then(function () {
       return Promise.all([
         // Inserts seed entries
-        knex('users').insert({Name: 'Mandy', Email: 'mandy@email.com'}),
-        knex('users').insert({Name: 'Kirby', Email: 'kirby@email.com'}),
-        knex('users').insert({Name: 'Alannah', Email: 'alannah@email.com'}),
-        knex('users').insert({Name: 'Kate', Email: 'kate@email.com'})
+        knex('users').insert({id: 1, Name: 'Mandy', Email: 'mandy@email.com'}),
+        knex('users').insert({id: 2, Name: 'Kirby', Email: 'kirby@email.com'}),
+        knex('users').insert({id: 3, Name: 'Alannah', Email: 'alannah@email.com'}),
+        knex('users').insert({id: 4, Name: 'Kate', Email: 'kate@email.com'})
       ]);
     });
 };
